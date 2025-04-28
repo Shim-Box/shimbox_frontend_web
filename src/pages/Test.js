5a5e1086b05df70f7369c988a540e29ec4b57532
juzuhee <txtloverrr@naver.com>
2025-04-28 11:24:57 +0900

    feat: React 프로젝트 초기 업로드

---
+++ src/pages/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Test.module.css';
+
+function Test() {
+  return (
+    <div className={styles.container}>
+      testtttt
+    </div>
+  );
+}
+
+export default Test;
