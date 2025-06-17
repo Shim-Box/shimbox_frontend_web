7e6138e8a0cefa2950005b889c00131889567573
강태현 <0923kth@naver.com>
2025-06-17 12:33:08 +0900

    수정

--- src/models/AdminModels.ts
+++ src/models/AdminModels.ts
@@ -13,7 +13,7 @@ export interface AuthTokens {
 
 // 가입 대기자
 export interface PendingUser {
-  id: number; // 고유 ID
+  id: number;
   name: string;
   phoneNumber: string;
   residence: string;
@@ -29,8 +29,8 @@ export interface PendingUser {
 
 // 승인된 회원(택배기사)
 export interface ApprovedUser {
-  userId: number; // 유저 ID
-  driverId: number; // 드라이버 ID (상세 조회 및 키로 사용)
+  userId: number;
+  driverId: number;
   name: string;
   approvalStatus: boolean;
   profileImageUrl: string;
@@ -48,21 +48,21 @@ export interface ApprovedUser {
 
 // 기사 퇴근 후 건강 데이터
 export interface DriverHealthData {
-  workTime: string; // 출근 시간 (ISO 형식)
-  leaveWorkTime: string; // 퇴근 시간 (ISO 형식)
-  finish1: string; // 설문 응답 1
-  finish2: string; // 설문 응답 2
-  finish3: string; // 설문 응답 3
-  step: number; // 걸음 수
-  heartRate: number; // 심박수
-  conditionStatus: string; // 상태 (위험, 불안, 좋음)
+  workTime: string;
+  leaveWorkTime: string;
+  finish1: string;
+  finish2: string;
+  finish3: string;
+  step: number;
+  heartRate: number;
+  conditionStatus: string;
 }
 
 // 관리자용 실시간 건강 데이터
 export interface DriverRealtimeData {
-  driverId: number; // 드라이버 ID
-  step: number; // 실시간 걸음 수
-  heartRate: number; // 실시간 심박수
-  conditionStatus: string; // 상태 (위험, 불안, 좋음)
-  modifiedDate: string; // 업데이트 시간 (ISO 형식)
+  driverId: number;
+  step: number;
+  heartRate: number;
+  conditionStatus: string;
+  modifiedDate: string;
 }
