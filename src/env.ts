a43a6f4779d1b49b9557ba6f06e08fac91cf8955
강태현 <0923kth@naver.com>
2025-11-11 14:12:16 +0900

    카카오 지도 앱키 수정

--- src/env.ts
+++ src/env.ts
@@ -1,1 +1,3 @@
 export const BASE_URL = "http://116.39.208.72:26443";
+export const KAKAO_JS_KEY = "73642be44ca8afbe83b942626590b1cd";
+export const KAKAO_REST_KEY = "829199913fa226615a18f53b13738d3a";
